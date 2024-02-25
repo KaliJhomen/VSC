0e17595419824461a3743938d6c8e6fb3e4c7d7d
Ader <a1d4r1310@gmail.com>
2024-02-25 15:03:31 -0500

    {changes}

--- tempCodeRunnerFile.cpp
+++ tempCodeRunnerFile.cpp
@@ -1,43 +1,64 @@
 #include <iostream>
+
 using namespace std; 
-class humano {
-protected:
-    string nombre;
-    bool come;
-    bool duerme;
-    bool estudia;
+class animal{
+protected: 
+    string especie;
+    int edad;
+    int peso;
 public: 
-    humano(string, bool, bool, bool); //constructor de la clase humano
-    void mostrarHumano(){
-        string comeS = come ? "Si" : "No";
-        string duermeS = duerme ? "Si" : "No";
-        string estudiaS = estudia ? "Si" : "No";
-        cout<<"Nombre: "<<nombre<<endl<<"Come?: "<<comeS<<endl<<"Duerme?: "<<duermeS<<endl<<"Estudia?: "<<estudiaS<<endl;
+    animal(string, int, int); //constructor de la clase animal
+    void mostrarAnimal(){
+        cout<<"Especie: "<<especie<<endl<<"Edad: "<<edad<<" meses"<<endl<<"Peso: "<<peso<<" kg"<<endl;
     }
 };
-humano :: humano (string _nombre, bool _come, bool _duerme, bool _estudia= false){
-    nombre= _nombre;
-    come= _come;
-    duerme= _duerme;
+animal :: animal (string _especie, int _edad, int _peso){
+    especie= _especie;
+    edad= _edad;
+    peso= _peso;
 }
-class universitario : public humano{
+//subclase mamifero
+class mamifero : public animal{
+private: 
+    int npezones;
 public: 
-    universitario(string, bool, bool, bool);
-    void mostrarUniversitario(){
-        mostrarHumano();
+    mamifero(string, int, int, int); //constructor de la subclase mamifero
+    void mostrarMamifero(){
+        mostrarAnimal();
+        cout<<"Numero de pezones: "<<npezones<<endl;
     }
 };
-universitario :: universitario (string _nombre, bool _come, bool _duerme, bool _estudia) : humano (_nombre, _come, _duerme, _estudia ){
+mamifero :: mamifero (string _clase, int _edad, int _peso, int _npezones) : animal (_clase, _edad, _peso){
+    npezones= _npezones;
 }
 
 int main(){
-    universitario universitario1 ("Soberbio", 0, 0, 1);
-    cout <<"Universitario:"<<endl;
-    universitario1.mostrarUniversitario();
+    //prueba con datos preespecificados
+    mamifero mamifero1("Gato", 5, 18, 8);   
+    mamifero1.mostrarMamifero();
 
-    humano humano1 ("Luchito", 1, 1);
-    cout<<endl<<"Persona:"<<endl;
-    humano1.mostrarHumano(); 
-
-    return 0;
+    char esmamifero;
+    string especie;
+    int edad;
+    int peso;
+    cout<<"Ingrese la especie: "<<endl;
+    cin>>especie;
+    cout<<"Ingrese la edad: (en meses)"<<endl;
+    cin>>edad;
+    cout<<"Ingrese el peso: (en kg)"<<endl;
+    cin>>peso;
+    cout<<"Es mamifero?: (Y/N)"<<endl;
+    cin>>esmamifero;
+    if (esmamifero== 'Y'|| esmamifero=='y'){
+        int npezones;
+        cout<<"Ingrese el numero de pezones: "<<endl;
+        cin>>npezones;
+        mamifero mamifero2(especie, edad, peso, npezones);
+        mamifero2.mostrarMamifero();
+    }
+    else {
+    animal animal1(especie, edad, peso);
+    animal1.mostrarAnimal();
+    }
+    return 0; 
 }
